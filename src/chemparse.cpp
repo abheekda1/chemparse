5f36fd2b58a84f6d0863a7fd851f2a3a78a7ecc2
Abheek Dhawan <abheekd@protonmail.com>
2022-02-25 16:26:21 -0600

    Format with clang-format

--- src/chemparse.cpp
+++ src/chemparse.cpp
@@ -3,150 +3,155 @@
 #include <fstream>
 
 bool checkCapitalCase(char currentChar) {
-    if (toupper(currentChar) == currentChar) {
-        return true;
-    }
-    return false;
+  if (toupper(currentChar) == currentChar) {
+    return true;
+  }
+  return false;
 }
 
 namespace chemparse {
-    std::vector<Element> parseFormulaToElements(std::string chemFormula) {
-        std::vector<Element> elements;
+std::vector<Element> parseFormulaToElements(std::string chemFormula) {
+  std::vector<Element> elements;
 
-        for (int i = 0; i < chemFormula.size(); i ++) {
-            char currentChar = chemFormula[i];
-            char nextChar = chemFormula[i + 1];
+  for (int i = 0; i < chemFormula.size(); i++) {
+    char currentChar = chemFormula[i];
+    char nextChar = chemFormula[i + 1];
 
-            Element element;
+    Element element;
 
-            // Make sure the current character is capital to prevent starting with a lowercase letter
-            if (checkCapitalCase(currentChar)) {
-                // If current character is capital and the next one isn't, they're one element
-                if (!checkCapitalCase(nextChar) && i < chemFormula.size() - 1) {
-                    if (!isdigit(nextChar)) {
-                        element.symbol = currentChar;
-                        element.symbol += nextChar;
+    // Make sure the current character is capital to prevent starting with a
+    // lowercase letter
+    if (checkCapitalCase(currentChar)) {
+      // If current character is capital and the next one isn't, they're one
+      // element
+      if (!checkCapitalCase(nextChar) && i < chemFormula.size() - 1) {
+        if (!isdigit(nextChar)) {
+          element.symbol = currentChar;
+          element.symbol += nextChar;
 
-                        // Make sure it's less than two less the length of the array to prevent accessing something outside
-                        if (i < chemFormula.size() - 2) {
-                            if (isdigit(chemFormula[i + 2])) {
-                                std::string amtString;
-                                int numLength = 0;
-                                for (int j = i + 2; j < chemFormula.size(); j++) {
-                                    if (isdigit(chemFormula[j])) {
-                                        amtString += chemFormula[j];
-                                        numLength++;
-                                    } else {
-                                        break;
-                                    }
-                                }
-                                element.amount = stol(amtString);
-                                i += numLength;
-                            } else {
-                                element.amount = 1;
-                            }
-                        } else {
-                            element.amount = 1;
-                        }
-
-                        // Increment i to skip next char since it's part of this element (just speeds it up a bit -- not necessary)
-                        i++;
-                    } else {
-                        std::string amtString;
-                        int numLength = 0;
-                        for (int j = i + 1; j < chemFormula.size(); j++) {
-                            if (isdigit(chemFormula[j])) {
-                                amtString += chemFormula[j];
-                                numLength++;
-                            } else {
-                                break;
-                            }
-                        }
-                        element.amount = stol(amtString);
-                        i += numLength;
-                    }
-                    // Otherwise, this one is its own element
+          // Make sure it's less than two less the length of the array to
+          // prevent accessing something outside
+          if (i < chemFormula.size() - 2) {
+            if (isdigit(chemFormula[i + 2])) {
+              std::string amtString;
+              int numLength = 0;
+              for (int j = i + 2; j < chemFormula.size(); j++) {
+                if (isdigit(chemFormula[j])) {
+                  amtString += chemFormula[j];
+                  numLength++;
                 } else {
-                    element.symbol = currentChar;
-
-                    // Make sure it's less than two less the length of the array to prevent accessing something outside
-                    if (i < chemFormula.size() - 1) {
-                        if (isdigit(chemFormula[i + 1])) {
-                            std::string amtString;
-                            int numLength = 0;
-                            for (int j = i + 1; j < chemFormula.size(); j++) {
-                                if (isdigit(chemFormula[j])) {
-                                    amtString += chemFormula[j];
-                                    numLength++;
-                                } else {
-                                    break;
-                                }
-                            }
-                            element.amount = stol(amtString);
-                            i += numLength;
-                            //element.amount = chemFormula[i + 1] - '0' /*convert to int from val stored in char*/;
-                            //i++;
-                        } else {
-                            element.amount = 1;
-                        }
-                    } else {
-                        element.amount = 1;
-                    }
+                  break;
                 }
-                bool foundElement = populateElement(element);
-                if (!foundElement) {
-                    elements.clear();
-                    break;
-                } else {
-                    elements.push_back(element);
-                }
+              }
+              element.amount = stol(amtString);
+              i += numLength;
+            } else {
+              element.amount = 1;
             }
-        }
-        return elements;
-    }
+          } else {
+            element.amount = 1;
+          }
 
-    bool populateElement(Element &element) {
-        std::string symbol = element.symbol;
-        long amount = element.amount;
-
-        std::ifstream f("periodic-table.json");
-        std::string periodicTableData;
-
-        if (f) {
-            std::stringstream ss;
-            ss << f.rdbuf();
-            periodicTableData = ss.str();
+          // Increment i to skip next char since it's part of this element (just
+          // speeds it up a bit -- not necessary)
+          i++;
         } else {
-            std::cout << "ERROR: Missing the periodic table JSON file!" << std::endl;
-            exit(1);
-        }
-
-        nlohmann::json elementData = nlohmann::json::parse(periodicTableData)["elements"];
-        bool foundElement = false;
-        for (int i = 0; i < elementData.size(); i++) {
-            if (elementData[i]["symbol"] == symbol) {
-                nlohmann::json currentElementData = elementData[i];
-                element.name = currentElementData.value("name", "");
-                element.molarMass = currentElementData.value("atomic_number", 0);
-                element.atomicMass = currentElementData.value("atomic_mass", 0.0);
-                element.molarMass = currentElementData.value("atomic_mass", 0.0);
-                element.atomicNumber = currentElementData.value("number", 0);
-                element.appearance = currentElementData.value("appearance", "");
-                element.discoverer = currentElementData.value("discovered_by", "");
-                foundElement = true;
-                break;
+          std::string amtString;
+          int numLength = 0;
+          for (int j = i + 1; j < chemFormula.size(); j++) {
+            if (isdigit(chemFormula[j])) {
+              amtString += chemFormula[j];
+              numLength++;
+            } else {
+              break;
             }
+          }
+          element.amount = stol(amtString);
+          i += numLength;
         }
-        return foundElement;
-    }
+        // Otherwise, this one is its own element
+      } else {
+        element.symbol = currentChar;
 
-    Compound::Compound(std::vector<Element> elmnts) : elements(elmnts) {}
-
-    double Compound::getMolarMass() {
-        double molarMass = 0.0;
-        for (chemparse::Element element : elements) {
-            molarMass += double(element.amount) * element.molarMass;
+        // Make sure it's less than two less the length of the array to prevent
+        // accessing something outside
+        if (i < chemFormula.size() - 1) {
+          if (isdigit(chemFormula[i + 1])) {
+            std::string amtString;
+            int numLength = 0;
+            for (int j = i + 1; j < chemFormula.size(); j++) {
+              if (isdigit(chemFormula[j])) {
+                amtString += chemFormula[j];
+                numLength++;
+              } else {
+                break;
+              }
+            }
+            element.amount = stol(amtString);
+            i += numLength;
+            // element.amount = chemFormula[i + 1] - '0' /*convert to int from
+            // val stored in char*/; i++;
+          } else {
+            element.amount = 1;
+          }
+        } else {
+          element.amount = 1;
         }
-        return molarMass;
+      }
+      bool foundElement = populateElement(element);
+      if (!foundElement) {
+        elements.clear();
+        break;
+      } else {
+        elements.push_back(element);
+      }
     }
+  }
+  return elements;
 }
+
+bool populateElement(Element &element) {
+  std::string symbol = element.symbol;
+
+  std::ifstream f("periodic-table.json");
+  std::string periodicTableData;
+
+  if (f) {
+    std::stringstream ss;
+    ss << f.rdbuf();
+    periodicTableData = ss.str();
+  } else {
+    std::cout << "ERROR: Missing the periodic table JSON file!" << std::endl;
+    exit(1);
+  }
+
+  nlohmann::json elementData =
+      nlohmann::json::parse(periodicTableData)["elements"];
+  bool foundElement = false;
+  for (int i = 0; i < elementData.size(); i++) {
+    if (elementData[i]["symbol"] == symbol) {
+      nlohmann::json currentElementData = elementData[i];
+      element.name = currentElementData.value("name", "");
+      element.molarMass = currentElementData.value("atomic_number", 0);
+      element.atomicMass = currentElementData.value("atomic_mass", 0.0);
+      element.molarMass = currentElementData.value("atomic_mass", 0.0);
+      element.atomicNumber = currentElementData.value("number", 0);
+      element.appearance = currentElementData.value("appearance", "");
+      element.discoverer = currentElementData.value("discovered_by", "");
+      foundElement = true;
+      break;
+    }
+  }
+  return foundElement;
+}
+
+Compound::Compound(std::vector<Element> elmnts) : elements(elmnts) {}
+
+double Compound::getMolarMass() {
+  double molarMass = 0.0;
+  for (chemparse::Element element : elements) {
+    molarMass += double(element.amount) * element.molarMass;
+  }
+  return molarMass;
+}
+} // namespace chemparse
--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,62 @@
 #include <climits>
 
 void waitForExit() {
-    std::cout << std::endl;
-    std::cout << "PLEASE PRESS ENTER OR CLOSE THE WINDOW TO TERMINATE THE PROGRAM:" << std::endl;
-    std::cin.clear(); std::cin.ignore(INT_MAX, '\n');
-    std::cin.get();
+  std::cout << std::endl;
+  std::cout
+      << "PLEASE PRESS ENTER OR CLOSE THE WINDOW TO TERMINATE THE PROGRAM:"
+      << std::endl;
+  std::cin.clear();
+  std::cin.ignore(INT_MAX, '\n');
+  std::cin.get();
 }
 
 int main() {
-    while (true) {
-        std::cout << "> ";
+  while (true) {
+    std::cout << "> ";
 
-        std::string chemFormula;
-        std::cin >> chemFormula;
+    std::string chemFormula;
+    std::cin >> chemFormula;
 
-        if (chemFormula == "exit") {
-            std::cout << std::endl << "Exiting..." << std::endl;
-            exit(0);
-        }
+    if (chemFormula == "exit") {
+      std::cout << std::endl << "Exiting..." << std::endl;
+      exit(0);
+    }
 
-        std::vector<chemparse::Element> elements = chemparse::parseFormulaToElements(chemFormula);
-        if (elements.size() == 0) {
-            std::cout << "Invalid or empty formula." << std::endl;
-            continue;
-        } else if (elements.size() == 1) {
-            chemparse::Element element = elements[0];
-            std::cout << element.name << " (" << element.symbol << ")" << ": " << std::endl
+    std::vector<chemparse::Element> elements =
+        chemparse::parseFormulaToElements(chemFormula);
+    if (elements.size() == 0) {
+      std::cout << "Invalid or empty formula." << std::endl;
+      continue;
+    } else if (elements.size() == 1) {
+      chemparse::Element element = elements[0];
+      std::cout << element.name << " (" << element.symbol << ")"
+                << ": " << std::endl
                 << "Atomic Number: " << element.atomicNumber << std::endl
                 << "Atomic Mass: " << element.atomicMass << " amu" << std::endl
                 << "Molar Mass: " << element.molarMass << " g/mol" << std::endl
                 << "Discoverer: " << element.discoverer << std::endl
                 << "Appearance: " << element.appearance << std::endl
-                << std::endl << "(HINT: 0 or empty values mean unknown or simply not in this database)"
+                << std::endl
+                << "(HINT: 0 or empty values mean unknown or simply not in "
+                   "this database)"
                 << std::endl;
-        } else {
-            for (chemparse::Element &element: elements) {
-                long amount = element.amount;
-                std::string symbol = element.symbol;
-                std::string name = element.name;
-                std::string isOrAre = (amount == 1 ? "is" : "are");
-                std::cout << "There " << isOrAre << " " << amount << " " << name << " (" << symbol << ")" << std::endl;
-            }
+    } else {
+      for (chemparse::Element &element : elements) {
+        long amount = element.amount;
+        std::string symbol = element.symbol;
+        std::string name = element.name;
+        std::string isOrAre = (amount == 1 ? "is" : "are");
+        std::cout << "There " << isOrAre << " " << amount << " " << name << " ("
+                  << symbol << ")" << std::endl;
+      }
 
-            chemparse::Compound compound(elements);
-            compound.getMolarMass();
-            std::cout << std::endl;
-            std::cout << "The total molar mass is: " << compound.getMolarMass() << " g/mol" << std::endl;
-            std::cout << "The total atomic mass is: " << compound.getMolarMass() << " amu" << std::endl;
-        }
+      chemparse::Compound compound(elements);
+      compound.getMolarMass();
+      std::cout << std::endl;
+      std::cout << "The total molar mass is: " << compound.getMolarMass()
+                << " g/mol" << std::endl;
+      std::cout << "The total atomic mass is: " << compound.getMolarMass()
+                << " amu" << std::endl;
     }
+  }
 }
