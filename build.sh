7968da72b5e866e3568526b3ffb7e53686b9fe84
Abheek Dhawan <abheekd@protonmail.com>
2022-02-25 16:43:47 -0600

    Use CMake for copying file instead of build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Create build directory, without producing an error if it already exists
-mkdir -p build/
-cd build/
-cmake ..
-make
-cp ../periodic-table.json ./bin
-echo "Copied periodic table JSON to bin directory"
